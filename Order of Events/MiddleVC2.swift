bd518b6427d109d3f2fe8f5e7e327f6b86fad1c8
wangsh65 <wangsh@ssfs.org>
2024-06-03 14:42:34 -0400

    6/3

---
+++ Order of Events/MiddleVC2.swift
@@ -0,0 +1,29 @@
+//
+//  MiddleVC2.swift
+//  Order of Events
+//
+//  Created by Shelly on 6/3/24.
+//
+
+import UIKit
+
+class MiddleVC2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
