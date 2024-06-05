ec6d2fa316afd82477e712999c0748482463717e
wangsh65 <wangsh@ssfs.org>
2024-06-05 08:47:39 -0400

    pp

--- Order of Events/MiddleVC2.swift
+++ Order of Events/MiddleVC2.swift
@@ -9,8 +9,41 @@ import UIKit
 
 class MiddleVC2: UIViewController {
 
+
+
+    @IBOutlet weak var MiddleViewController: UILabel!
+    
+    var eventNumber: Int =  1
+    
+    func addEvent(from: String) {
+        if let existingText = MiddleViewController.text {
+            MiddleViewController.text = "\(existingText)\nEvent number \(eventNumber) was \(from)"
+            eventNumber += 1
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        addEvent(from: "viewDidLoad")
+
+    }
+    
+    override func viewWillAppear( _ animated : Bool) {
+        super.viewWillAppear(animated)
+        addEvent(from: "viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated : Bool){
+        super.viewDidAppear(animated)
+        addEvent(from: "viewDidAppear")
+    }
+    override func viewWillDisappear( _ animated : Bool){
+        super.viewWillDisappear(animated)
+        addEvent(from: "viewWillDisappear")
+    }
+    override func viewDidDisappear( _ animated : Bool){
+        super.viewDidDisappear(animated)
+        addEvent(from: "viewDidDisappearr")
+    }
 
         // Do any additional setup after loading the view.
     }
@@ -26,4 +59,3 @@ class MiddleVC2: UIViewController {
     }
     */
 
-}
